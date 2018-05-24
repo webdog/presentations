2fd14419d9fc4584b8bc8a694955d5ad0ead79af
Christian Weber <webdog@github.com>
2018-05-24 10:32:07 -0500

    add route for demo and the slides

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,24 @@
 from flask import render_template
+
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html', title='webdog')
+
+
+@app.route('/presentations')
+def hello():
+    return render_template('demo.html')
+
+
+@app.route('/presentations/<slides>')
+def presentation(slides):
+    html = ''
+    presopath = 'app/static/presentations/' + slides
+    with open(presopath, 'r') as f:
+        html = f.read()
+    return render_template('preso.html', html=html)
+
