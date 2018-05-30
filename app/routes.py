c2c78270155dc8ebb8a30510f9e9fb563e180341
Christian Weber <webdog@github.com>
2018-05-30 17:39:30 -0500

    added support for parsing of custom themes in static presentations

--- app/routes.py
+++ app/routes.py
@@ -19,6 +19,13 @@ def presentation(slides):
     html = ''
     presopath = 'app/static/presentations/' + slides
     with open(presopath, 'r') as f:
-        html = f.read()
-    return render_template('preso.html', html=html)
-
+        template_definition = f.readline()
+        if '!--' in template_definition:
+            # This a total hack, will fix later - webdog
+            # Custom presentation theme defined in first line of static presentation file
+            theme = template_definition.split(': ')[1].replace(' -->', '')
+            html = f.read()
+            return render_template('preso.html', html=html, theme=theme)
+        else:
+            html = f.read()
+            return render_template('preso.html', html=html)
