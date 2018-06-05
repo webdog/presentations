1db0ee31695dea1771f3404f2a574dbc9acb90b3
Christian Weber <webdog@github.com>
2018-06-04 20:28:39 -0700

    Load octicons, custom svg sizing, and pass inline octicons to view

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template
-
+from helpers.svg import SVG
 from app import app
-
+import os
 
 @app.route('/')
 @app.route('/index')
@@ -18,6 +18,20 @@ def hello():
 def presentation(slides):
     html = ''
     presopath = 'app/static/presentations/' + slides
+    oct_path = os.listdir('app/static/octicons/svg')
+    octicons = {}
+    for o in oct_path:
+        path = f'app/static/octicons/svg/{o}'
+        with open(path, 'r') as f:
+            #jinja does not support dashes in variables. change to dash to reference octicons
+            key = o.split('.')[0].replace('-', '_')
+            octicons[key] = f.readline()
+
+    footer_logo = SVG(octicons['mark_github'])
+    footer_logo  = footer_logo.resize(48, 48)
+
+
+
     with open(presopath, 'r') as f:
         template_definition = f.readline()
         if '!--' in template_definition:
@@ -25,7 +39,7 @@ def presentation(slides):
             # Custom presentation theme defined in first line of static presentation file
             theme = template_definition.split(': ')[1].replace(' -->', '')
             html = f.read()
-            return render_template('preso.html', html=html, theme=theme)
+            return render_template('preso.html', html=html, theme=theme, octicons=octicons, footer_logo=footer_logo)
         else:
             html = f.read()
-            return render_template('preso.html', html=html)
+            return render_template('preso.html', html=html, octicons=octicons)
